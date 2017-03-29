32704be63451ee80d547ce868cb993d8756542b7
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-03-29 14:15:23 -0400

    ex5

---
+++ exercise5.rb
@@ -0,0 +1,16 @@
+# Exercise 5: Greet Backwards
+
+# puts "Greetings, what is your name?"
+# name = gets.chomp
+# puts "Hello, #{name.reverse}! Welcome home!"
+
+
+def greet_backwards(name)
+  puts "Hello, #{name.reverse}! Welcome home!"
+end
+
+greet_backwards('AmandaAmanda')
+greet_backwards('BobBob')
+greet_backwards('ShirlyShirly')
+greet_backwards('SueSue')
+greet_backwards('AndyAndy')
