f79fb2820f1bf4c6177a1daca86e3b7898ea74dc
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-03-29 15:37:50 -0400

    temp_converter

---
+++ exercise5.rb
@@ -0,0 +1,15 @@
+# Fahrenhit to Celsius Calculator
+
+
+
+def temp_converter
+
+  puts "What is the temperature in Fahrenhit?"
+  temp = gets.to_i
+  (temp - 32) * 5/9
+
+end
+
+x = temp_converter
+
+puts x
