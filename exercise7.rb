21911ce1da962be68eec80a8d6a0599dc8b61924
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-03-29 16:09:59 -0400

    ex7

---
+++ exercise7.rb
@@ -0,0 +1,24 @@
+# Text Wrap
+
+def text_wrap
+  "---"
+end
+
+def text_wrap2
+  "==="
+end
+
+def text_wrap3
+  "###"
+end
+
+def text_wrap4
+  "new message"
+end
+
+q = text_wrap
+w = text_wrap2
+e = text_wrap3
+r = text_wrap4
+
+print q, w, e, r, e, w, q
