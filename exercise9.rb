f97e70db92616529061e8b4a748ad5c15d0b4b8d
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-03-29 20:33:06 -0400

    add rice

--- exercise9.rb
+++ exercise9.rb
@@ -1,7 +1,40 @@
 # Groceries
 
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+def print_list(paul)
+  paul.each do |item|
+    puts "* " + item
+  end
 
-grocery_list.each do |item|
-      puts "* " + item
 end
+
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+print_list(grocery_list)
+
+grocery_list << "rice"
+print_list(grocery_list)
+
+
+# def print_list(grocery_list)
+#   grocery_list.each do |item|
+#         puts "* " + item
+#       end
+# end
+#
+#
+# def add_item(f, g)
+#   g << f
+#   print_list(g)
+# end
+#
+#
+# grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+# print_list(grocery_list)
+#
+# puts "Would you like to add an item?"
+# answer = gets.chomp
+# if answer == "yes"
+# puts "enter item name"
+# forgot = gets.chomp
+# add_item(forgot, grocery_list)
+#
+# end
