6ea16c23da6dc9b24dd34cf8c4675dc1c4112aa0
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-03-29 22:16:23 -0400

    ex10

--- exercise10.rb
+++ exercise10.rb
@@ -4,6 +4,19 @@ students = {
   :cohort3 => 22
 }
 
+students[:cohort4] = "43"
+
+
 students.each do |c, v|
   puts "#{c}: #{v} students"
 end
+
+puts students.keys
+
+
+students.each do |c, v|
+  puts students[c] *= 1.05
+end
+
+  students.delete(:cohort2)
+  puts students
